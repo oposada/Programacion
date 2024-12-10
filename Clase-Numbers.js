9196f366af2d7929f6beb1af2191342090347570
Oscar Omar Posada Sánchez <oscar.posada.sanchez@gmail.com>
2024-12-09 20:38:35 -0600

    Mensaje de cambios v1.1

--- Clase-Numbers.js
+++ Clase-Numbers.js
@@ -1,1 +1,28 @@
 // mi primer archivo js web para mscode web .dev 
+// tipo de entero y dewcimal 
+const entero = 42
+const decimal = 3.14
+console.log (typeof entero, typeof decimal)
+
+// notacion cientifica 
+const cientifico = 5e3
+const infonito = Infinity
+
+// no numeros
+const noesunnumero = NaN
+
+// operaciones aritmeritas 
+const suma = 3 + 4
+const resta = 4 - 4
+const multiplicacion = 32 * 2
+const divison = 32 / 2
+
+// mudulo y exponenciacion 
+const modulo = 15 % 8
+const exponenciacion = 2 ** 3
+
+// presicion 
+const resultado = 0.1 + 0.2
+console.log(resultado)
+console.log(resultado.toFixed(1))
+console.log(resultado === 0.3)
